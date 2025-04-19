eaab78d9ba5524539b02ebda752d4fdc6d40acfd
William <cooki@Williams-MacBook-Pro.local>
2025-04-19 12:10:30 +1000

    feat: PoC deployment

--- script/DeployPortProofOfConcept.s.sol
+++ script/DeployPortProofOfConcept.s.sol
@@ -9,7 +9,7 @@ import { RolesAuthority, Authority } from "@solmate/auth/authorities/RolesAuthor
 import { TellerWithMultiAssetSupport } from "src/base/Roles/TellerWithMultiAssetSupport.sol";
 import { AccountantWithRateProviders } from "src/base/Roles/AccountantWithRateProviders.sol";
 import { AtomicSolverV3, AtomicQueue } from "src/atomic-queue/AtomicSolverV3.sol";
-import {MainnetAddresses} from "test/resources/MainnetAddresses.sol";
+import { MainnetAddresses } from "test/resources/MainnetAddresses.sol";
 
 import "@forge-std/Script.sol";
 
@@ -36,7 +36,7 @@ contract DeployPortProofOfConceptScript is Script, MainnetAddresses {
     AtomicSolverV3 public atomicSolverV3;
 
     ERC20 public USDX = ERC20(0xe29f6fbc4CB3F01e2D38F0Aab7D8861285EE9C36);
-    
+
     address public owner = 0xe43420E1f83530AAf8ad94e6904FDbdc3556Da2B;
     address public hexTrust = 0xe43420E1f83530AAf8ad94e6904FDbdc3556Da2B;
     address public solver = 0xe43420E1f83530AAf8ad94e6904FDbdc3556Da2B;
@@ -70,7 +70,12 @@ contract DeployPortProofOfConceptScript is Script, MainnetAddresses {
 
         rolesAuthority.setRoleCapability(MINTER_ROLE, address(boringVault), BoringVault.enter.selector, true);
         rolesAuthority.setRoleCapability(BURNER_ROLE, address(boringVault), BoringVault.exit.selector, true);
-        rolesAuthority.setRoleCapability(MANAGER_ROLE, address(boringVault), bytes4(keccak256(abi.encodePacked("manage(address,bytes,uint256)"))), true);
+        rolesAuthority.setRoleCapability(
+            MANAGER_ROLE,
+            address(boringVault),
+            bytes4(keccak256(abi.encodePacked("manage(address,bytes,uint256)"))),
+            true
+        );
         rolesAuthority.setRoleCapability(
             ADMIN_ROLE, address(teller), TellerWithMultiAssetSupport.addAsset.selector, true
         );
@@ -125,7 +130,7 @@ contract RemoveAssetsFromVaultScript is Script {
 
     function run() external {
         vm.startBroadcast(owner);
-        
+
         /// @dev move assets in and out arbitrarily if caller has authority
         address target = address(USDX);
         bytes memory data = abi.encodeCall(ERC20.transfer, (owner, 8e18));
